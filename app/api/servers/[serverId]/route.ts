6079edfa0c5754ff13ef419ed5e438db8c084971
RajaGeorge <rajageorge971@gmail.com>
2023-10-15 23:00:53 +0100

    *started Manage members functionality.

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -8,6 +8,7 @@ export async function PATCH(
 ) {
   try {
     const profile = await currentProfile();
+    const { name, imageUrl } = await req.json();
 
     if (!profile) {
       return new NextResponse("Unauthorized", { status: 401 });
@@ -15,9 +16,15 @@ export async function PATCH(
     const server = await db.server.update({
       where: {
         id: params.serverId,
+        profileId: profile.id,
       },
-      data: {},
+      data: {
+        name,
+        imageUrl,
+      },
     });
+
+    return NextResponse.json(server);
   } catch (error) {
     console.log("[SERVER_ID_PATCH]", error);
     return new NextResponse("Internal Error", { status: 500 });
